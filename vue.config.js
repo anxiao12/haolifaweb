521588287d850416ce3aff295282439141626367
chenjianbo <chenjianbo@ysstech.com>
2023-09-22 10:10:48 +0800

    first time npm install

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ module.exports = {
     port: 8082,
     proxy: {
       '/haolifa': {
-        target: 'http://127.0.0.1/',
-        // target: 'http://110.41.14.123:8080/',
+        // target: 'http://127.0.0.1/',
+        target: 'http://110.41.14.123:8080/',
         changeOrigin: true
       }
     },
