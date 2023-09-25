8a352c1f900ce2a5870d5b2a9a333a914cc192a7
chenjianbo <chenjianbo@ysstech.com>
2023-09-25 11:16:37 +0800

    save

--- src/pages/applyBuy/machineToPurchase/index.vue
+++ src/pages/applyBuy/machineToPurchase/index.vue
@@ -41,8 +41,9 @@
                     <td>{{item.productName}}</td>
                     <td>{{item.productNo}}</td>
                     <td>{{item.productOrderNo}}</td>
-                    <td>{{dealStatus(item.status)}}</td>
-                    <!-- <td>{{item.arrivalTime}}</td>
+                    <td>{{allStatus[item.status].text}}</td>
+                    <!-- <td>{{dealStatus(item.status)}}</td>
+                    <td>{{item.arrivalTime}}</td>
                     <td>{{allStatus[item.status].text}}</td>
                     <td class="t-right">
                         <a href="javascript:;" v-if="item.status==2" style="margin-right: 3px" class="blue" @click="dealApplyBuy(item.id)">处理完成</a>
