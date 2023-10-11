13c39d4302cef6a94e2590a85f4063fefea97a4d
chenjianbo <chenjianbo@ysstech.com>
2023-10-11 11:29:59 +0800

    save

--- src/pages/Order/list/edit.vue
+++ src/pages/Order/list/edit.vue
@@ -803,6 +803,8 @@ export default {
                 { value: 12, text: "申请发货" },
                 { value: 13, text: "发货完成" },
                 { value: 14, text: "审核不通过" },
+                { value: 15, text: "采购中" },
+
             ],
             newOrderStatusList: [
                 { value: 0, text: "创建" },
@@ -820,6 +822,8 @@ export default {
                 // { value: 12, text: "申请发货" },
                 // { value: 13, text: "发货完成" },
                 { value: 14, text: "审核不通过" },
+                { value: 15, text: "采购中" },
+
             ],
             filter: {
                 orderNo: "",
