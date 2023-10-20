6f1eeb4889a65bc73106b339dda45fd10dffcdd5
张安平 <zhanganping@lczq.com>
2023-10-20 10:36:51 +0800

    生产日计划BUG修复

--- src/pages/scrjh/detail.vue
+++ src/pages/scrjh/detail.vue
@@ -1,70 +1,28 @@
 <template>
     <div class="page-jishu-list">
-        <i
-            class="icon icon-abs"
-            @click="flush"
-        >autorenew</i>
+        <i class="icon icon-abs" @click="flush">autorenew</i>
         <div class="flex-v-center tool-bar">
-            <div
-                class="flex-v-center search-bar"
-                style="margin-right: 20px;"
-            >
+            <div class="flex-v-center search-bar" style="margin-right: 20px;">
                 <i class="icon f-20 c-8">search</i>
-                <input
-                    type="text"
-                    class="flex-item"
-                    v-model="filter.orderNo"
-                    @change="$refs.list.update(true)"
-                    placeholder="订单号"
-                    style="width: 200px;"
-                >
+                <input type="text" class="flex-item" v-model="filter.orderNo" @change="$refs.list.update(true)"
+                    placeholder="订单号" style="width: 200px;">
                 计划日期：
-                <el-date-picker
-                    v-model="filter.planDate"
-                    type="date"
-                    value-format="yyyy-MM-dd"
-                    @change="$refs.list.update(true);"
-                    :editable="false"
-                    placeholder="选择年月日"
-                    style="width: 200px;"
-                ></el-date-picker>
+                <el-date-picker v-model="filter.planDate" type="date" value-format="yyyy-MM-dd"
+                    @change="$refs.list.update(true);" :editable="false" placeholder="选择年月日"
+                    style="width: 200px;"></el-date-picker>
                 计划状态：
-                <select
-                    v-model="filter.planStatus"
-                    class="f-14"
-                    @change="$refs.list.update(true)"
-                >
+                <select v-model="filter.planStatus" class="f-14" @change="$refs.list.update(true)">
                     <option value="">全部</option>
-                    <option
-                        v-for="item,index in statusList"
-                        :value="item.value"
-                        :key="index"
-                    >{{item.text}}</option>
+                    <option v-for="item, index in statusList" :value="item.value" :key="index">{{ item.text }}</option>
                 </select>
             </div>
             <div class="flex-item"></div>
-            <btn
-                class="b"
-                flat
-                color="#008eff"
-                @click="add"
-            >新增</btn>
-            <btn
-                class="b"
-                flat
-                color="#008eff"
-                @click="back"
-            >返回</btn>
+            <btn class="b" flat color="#008eff" @click="add">新增</btn>
+            <btn class="b" flat color="#008eff" @click="back">返回</btn>
         </div>
         <div class="flex-item scroll-y">
-            <data-list
-                ref="list"
-                :page-size="15"
-                :param="filter"
-                url="/haolifa/production-daily-plan/detail/page"
-                method="post"
-                :borderFlag="true"
-            >
+            <data-list ref="list" :page-size="15" :param="filter" url="/haolifa/production-daily-plan/detail/page"
+                method="post" :borderFlag="true">
                 <tr slot="header">
                     <th style="width: 60px;">序号</th>
                     <th>订单号</th>
@@ -76,180 +34,83 @@
                     <th>实际完成日期</th>
                     <th>状态</th>
                     <th>备注</th>
-                    <th
-                        class="t-right"
-                        style="width: 80px;"
-                    >操作</th>
+                    <th class="t-right" style="width: 80px;">操作</th>
                 </tr>
-                <template
-                    slot="item"
-                    slot-scope="{ item,index }"
-                >
+                <template slot="item" slot-scope="{ item,index }">
                     <td class="c-a">{{ index }}</td>
-                    <td>{{item.orderNo}}</td>
-                    <td>{{item.orderNumber}}</td>
-                    <td>{{item.finishNumber}}</td>
-                    <td>{{item.deliveryDate}}</td>
-                    <td>{{item.planDate}}</td>
-                    <td>{{item.planFinishDate}}</td>
-                    <td>{{item.actualFinishDate}}</td>
-                    <td>{{formatter(item.planStatus)}}</td>
-                    <td>{{item.remark}}</td>
+                    <td>{{ item.orderNo }}</td>
+                    <td>{{ item.orderNumber }}</td>
+                    <td>{{ item.finishNumber }}</td>
+                    <td>{{ item.deliveryDate }}</td>
+                    <td>{{ item.planDate }}</td>
+                    <td>{{ item.planFinishDate }}</td>
+                    <td>{{ item.actualFinishDate }}</td>
+                    <td>{{ formatter(item.planStatus) }}</td>
+                    <td>{{ item.remark }}</td>
                     <td class="t-right">
-                        <a
-                            href="javascript:;"
-                            class="blue"
-                            @click="edit(item)"
-                        >编辑 |</a>
-                        <a
-                            href="javascript:;"
-                            class="red"
-                            @click="del(item)"
-                        >删除</a>
+                        <a href="javascript:;" class="blue" @click="edit(item)">编辑 |</a>
+                        <a href="javascript:;" class="red" @click="del(item)">删除</a>
                     </td>
                 </template>
             </data-list>
         </div>
-        <layer
-            v-show="addLayer"
-            title="新增"
-            width="80%"
-        >
-            <div
-                class="layer-text"
-                style="padding-bottom: 50px;"
-            >
+        <layer v-show="addLayer" title="新增" width="80%">
+            <div class="layer-text" style="padding-bottom: 50px;">
                 <div class="flex">
-                    <input-box
-                        v-model="planDate"
-                        class="flex-item"
-                        label="计划日期"
-                        :disabled="true"
-                    ></input-box>
+                    <input-box v-model="planDate" class="flex-item" label="计划日期" :disabled="true"></input-box>
                 </div>
                 <div class="flex">
-                    <input-box
-                        v-model="remark"
-                        class="flex-item"
-                        label="备注"
-                    ></input-box>
+                    <input-box v-model="remark" class="flex-item" label="备注"></input-box>
                 </div>
                 <div class="flex">
                     <div class="flex-item mr-20">
 
-                        <label
-                            class="mt-5"
-                            for=""
-                        >订单状态:</label>
-                        <el-select
-                            v-model="param.orderStatus"
-                            placeholder="请选择"
-                            size="mini"
-                            class="ml-5"
-                            @change="orderStatusChange"
-                        >
-                            <el-option
-                                v-for="item in orderStatusList"
-                                :key="item.value"
-                                :label="item.text"
-                                :value="item.value"
-                            >
+                        <label class="mt-5" for="">订单状态:</label>
+                        <el-select v-model="param.orderStatus" placeholder="请选择" size="mini" class="ml-5"
+                            @change="orderStatusChange">
+                            <el-option v-for="item in orderStatusList" :key="item.value" :label="item.text"
+                                :value="item.value">
                             </el-option>
                         </el-select>
                     </div>
                     <div class="flex-item mr-20">
                         订单号:
-                        <el-input
-                            size="mini"
-                            type="text"
-                            style="width:60%"
-                            @change="orderStatusChange"
-                            v-model="param.orderNo"
-                        ></el-input>
+                        <el-input size="mini" type="text" style="width:60%" @change="orderStatusChange"
+                            v-model="param.orderNo"></el-input>
                     </div>
                     <div class="flex-item mr-20">
                         发货开始时间:
-                        <el-date-picker
-                            size="mini"
-                            v-model="param.deliveryStartDate"
-                            type="date"
-                            value-format="yyyy-MM-dd"
-                            @change="orderStatusChange"
-                            :editable="false"
-                            placeholder="选择年月日"
-                        ></el-date-picker>
+                        <el-date-picker size="mini" v-model="param.deliveryStartDate" type="date" value-format="yyyy-MM-dd"
+                            @change="orderStatusChange" :editable="false" placeholder="选择年月日"></el-date-picker>
                     </div>
                     <div class="flex-item mr-20">
                         发货结束时间:
-                        <el-date-picker
-                            size="mini"
-                            v-model="param.deliveryEndDate"
-                            type="date"
-                            value-format="yyyy-MM-dd"
-                            @change="orderStatusChange"
-                            :editable="false"
-                            placeholder="选择年月日"
-                        ></el-date-picker>
+                        <el-date-picker size="mini" v-model="param.deliveryEndDate" type="date" value-format="yyyy-MM-dd"
+                            @change="orderStatusChange" :editable="false" placeholder="选择年月日"></el-date-picker>
                     </div>
                 </div>
-                <div
-                    class="layer-text"
-                    style="padding-bottom: 50px;"
-                >
-                    <el-table
-                        :data="tableData"
-                        v-loading="loading"
-                        tooltip-effect="dark"
-                        style="width: 100%"
-                        ref="multipleTable"
-                        size="mini"
-                        :row-class-name="tableRowClassName"
-                        @selection-change="handleSelectionChange"
-                    >
-                        <el-table-column
-                            type="selection"
-                            width="55"
-                        >
+                <div class="layer-text" style="padding-bottom: 50px;">
+                    <el-table :data="tableData" v-loading="loading" tooltip-effect="dark" style="width: 100%"
+                        ref="multipleTable" size="mini" :row-class-name="tableRowClassName"
+                        @selection-change="handleSelectionChange">
+                        <el-table-column type="selection" width="55">
                         </el-table-column>
-                        <el-table-column
-                            type="index"
-                            width="50"
-                        >
+                        <el-table-column type="index" width="50">
                         </el-table-column>
-                        <el-table-column
-                            prop="orderNo"
-                            label="订单号"
-                        ></el-table-column>
-                        <el-table-column
-                            prop="demandName"
-                            label="需方"
-                        ></el-table-column>
-                        <el-table-column
-                            prop="totalCount"
-                            label="订单数量"
-                        ></el-table-column>
-                        <el-table-column
-                            prop="deliveryDate"
-                            label="发货日期"
-                        ></el-table-column>
-                        <el-table-column
-                            prop="createTime"
-                            label="创建时间"
-                        ></el-table-column>
+                        <el-table-column prop="orderNo" label="订单号"></el-table-column>
+                        <el-table-column prop="demandName" label="需方"></el-table-column>
+                        <el-table-column prop="totalCount" label="订单数量"></el-table-column>
+                        <el-table-column prop="deliveryDate" label="发货日期"></el-table-column>
+                        <el-table-column prop="createTime" label="创建时间"></el-table-column>
                         <el-table-column label="是否已经关联日计划">
                             <template slot-scope="scope">
-                                {{ scope.row.associationPlan==1?"是":"否" }}
+                                {{ scope.row.associationPlan == 1 ? "是" : "否" }}
                             </template>
                         </el-table-column>
                         <el-table-column label="计划完成日期">
                             <template slot-scope="scope">
-                                <el-date-picker
-                                    v-model="addForm[scope.row.number].planFinishDate"
-                                    type="date"
-                                    placeholder="选择日期"
-                                    value-format="yyyy-MM-dd"
-                                    size="mini"
-                                >
+                                <el-date-picker v-model="addForm[scope.row.number].planFinishDate" type="date"
+                                    placeholder="选择日期" value-format="yyyy-MM-dd" size="mini">
                                 </el-date-picker>
                             </template>
                         </el-table-column>
@@ -258,75 +119,29 @@
             </div>
 
             <div class="layer-btns">
-                <el-button
-                    class="mr-20"
-                    size="mini"
-                    :loading="addLoading"
-                    type="primary"
-                    @click="save"
-                >保存</el-button>
-                <el-button
-                    class="mr-20"
-                    size="mini"
-                    @click="addClose"
-                >关闭</el-button>
+                <el-button class="mr-20" size="mini" :loading="addLoading" type="primary" @click="save">保存</el-button>
+                <el-button class="mr-20" size="mini" @click="addClose">关闭</el-button>
             </div>
         </layer>
-        <layer
-            v-show="editLayer"
-            title="编辑"
-            width="50%"
-        >
-            <div
-                class="layer-text"
-                style="padding-bottom: 50px;"
-            >
+        <layer v-show="editLayer" title="编辑" width="50%">
+            <div class="layer-text" style="padding-bottom: 50px;">
                 <div class="flex">
-                    <input-box
-                        v-model="form.orderNo"
-                        class="flex-item"
-                        label="订单号"
-                        :disabled="true"
-                    ></input-box>
+                    <input-box v-model="form.orderNo" class="flex-item" label="订单号" :disabled="true"></input-box>
                 </div>
                 <div class="flex">
-                    <input-box
-                        v-model="form.planDate"
-                        class="flex-item"
-                        label="计划日期"
-                        :disabled="true"
-                    ></input-box>
+                    <input-box v-model="form.planDate" class="flex-item" label="计划日期" :disabled="true"></input-box>
                 </div>
                 <div class="flex">
-                    <date-picker
-                        v-model="form.planFinishDate"
-                        hint="必填"
-                        class="flex-item"
-                        label="计划完成日期"
-                    ></date-picker>
+                    <date-picker v-model="form.planFinishDate" hint="必填" class="flex-item" label="计划完成日期"></date-picker>
                 </div>
                 <div class="flex">
-                    <input-box
-                        v-model="form.remark"
-                        class="flex-item"
-                        label="备注"
-                    ></input-box>
+                    <input-box v-model="form.remark" class="flex-item" label="备注"></input-box>
                 </div>
             </div>
 
             <div class="layer-btns">
-                <el-button
-                    class="mr-20"
-                    size="mini"
-                    :loading="loading"
-                    type="primary"
-                    @click="editSave"
-                >保存</el-button>
-                <el-button
-                    class="mr-20"
-                    size="mini"
-                    @click="editClose"
-                >关闭</el-button>
+                <el-button class="mr-20" size="mini" :loading="loading" type="primary" @click="editSave">保存</el-button>
+                <el-button class="mr-20" size="mini" @click="editClose">关闭</el-button>
             </div>
         </layer>
     </div>
@@ -446,6 +261,7 @@ export default {
         getOrderList() {
             this.loading = true;
             this.tableData = [];
+            this.addForm = [];
             this.form = { payOrderUserRelationProcedureList: [] };
             this.$http
                 .post(`/haolifa/production-daily-plan/order-product/page`, this.param)
@@ -602,6 +418,7 @@ export default {
     .warning-row {
         color: #ef736f;
     }
+
     select {
         background: none;
         border: none;
@@ -609,17 +426,19 @@ export default {
         padding: 5px 20px 5px 10px;
         appearance: none;
     }
+
     .scroll-y {
         padding-bottom: 40px;
     }
-    .t-border{
+
+    .t-border {
         // tr{
-            border:1px solid #ccc
-        // }
+        border: 1px solid #ccc // }
     }
 
     //
 }
+
 .fixed-length {
     width: 100px;
     display: block;
@@ -627,6 +446,7 @@ export default {
     white-space: nowrap;
     text-overflow: ellipsis;
 }
+
 select {
     background: none;
     border: none;
@@ -634,28 +454,35 @@ select {
     padding: 5px 20px 5px 10px;
     appearance: none;
 }
+
 .page-supplier-info {
     padding: 30px 20px;
+
     tr:first-child td {
         padding: 0;
         border: none;
     }
+
     th {
         font-weight: normal;
         color: #888;
     }
+
     td {
         color: #444;
     }
+
     th,
     td {
         padding: 10px;
         border: 1px solid #fff;
         border: 1px solid #ddd;
     }
+
     .checkbox-list {
         flex-wrap: wrap;
     }
+
     .checkbox-item {
         line-height: 1em;
         width: 180px;
